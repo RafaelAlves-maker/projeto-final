0e9ef8d65721f30a286333d195824c66e530c505
RafaelAlves-maker <fael360alves@gmail.com>
2024-09-04 14:55:44 -0300

    Create funcoes.py

---
+++ funcoes.py
@@ -0,0 +1,39 @@
+import os
+
+# Lista que armazenará as tarefas
+tarefas = []
+
+def mostrar_tarefas(): #primeira função
+    """Exibe a lista de tarefas."""
+    if len(tarefas) == 0: 
+        print("Nenhuma tarefa na lista.")
+    else:
+        print("Tarefas:")
+        for i, tarefa in enumerate(tarefas, 1): 
+            print(f"{i}. {tarefa}")
+
+def adicionar_tarefa(): #segunda função
+    """Adiciona uma nova tarefa à lista."""
+    tarefa = input("Digite a nova tarefa: ")
+    tarefas.append(tarefa)
+    print(f"Tarefa '{tarefa}' adicionada com sucesso!")
+
+def remover_tarefa(): #terceira função
+    """Remove uma tarefa da lista."""
+    mostrar_tarefas()
+    if len(tarefas) > 0:
+        try:
+            indice = int(input("Digite o número da tarefa que deseja remover: "))
+            if 1 <= indice <= len(tarefas):
+                tarefa_removida = tarefas.pop(indice - 1)
+                print(f"Tarefa '{tarefa_removida}' removida com sucesso!")
+            else:
+                print("Número inválido!")
+        except ValueError:
+            print("Por favor, insira um número válido.")
+    else:
+        print("A lista está vazia. Nada para remover.")
+
+
+def limpar_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
