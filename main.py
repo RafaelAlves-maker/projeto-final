3e3eeea8589ce0e29fa37c997bf50eae120e791d
RafaelAlves-maker <fael360alves@gmail.com>
2024-09-04 14:56:44 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import funcoes
+
+
+
+while True:
+        print("\nMenu:")
+        print("1. Mostrar tarefas")
+        print("2. Adicionar tarefa")
+        print("3. Remover tarefa")
+        print("4. Sair")
+
+        escolha = input("Escolha uma opção: ")
+
+        if escolha == '1':
+            funcoes.mostrar_tarefas()
+        elif escolha == '2':
+            funcoes.adicionar_tarefa()
+        elif escolha == '3':
+            funcoes.remover_tarefa()
+        elif escolha == '4':
+            print("Saindo do programa...")
+            break
+        else:
+            print("Opção inválida! Tente novamente.")
+
+
+
+input("Pressione Enter para limpar o terminal...")
+funcoes.limpar_terminal()
